ab13ca5980d6487065034501f78304c213178851
Yitzchak M. Gottlieb <ygottlieb@perspectalabs.com>
2018-12-19 17:13:19 -0500

    Added LineSegment for line calculations.
    Added functions in height map to return segmented path on height
    boundaries.

--- src/com/perspectalabs/bonnmotion/util/HeightMap.java
+++ src/com/perspectalabs/bonnmotion/util/HeightMap.java
@@ -3,6 +3,7 @@
  */
 package com.perspectalabs.bonnmotion.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -11,9 +12,11 @@ import org.gdal.gdal.Band;
 import org.gdal.gdal.Dataset;
 import org.gdal.gdal.gdal;
 import org.gdal.gdalconst.gdalconst;
+import org.gdal.gdalconst.gdalconstConstants;
 import org.gdal.osr.CoordinateTransformation;
 import org.gdal.osr.SpatialReference;
 import org.gdal.osr.osr;
+import org.gdal.osr.osrConstants;
 
 import edu.bonn.cs.iv.bonnmotion.Position;
 import edu.bonn.cs.iv.util.maps.PositionGeo;
@@ -37,6 +40,7 @@ public class HeightMap {
     private CoordinateTransformation fromWgs84 = null;
 
     private double[] datasetInvTransform = null;
+    private double[] datasetTransform = null;
 
     private double noDataValue = Double.NaN;
 
@@ -97,6 +101,104 @@ public class HeightMap {
         return transformPosition(ct, position.x(), position.y());
     }
 
+    private class Corner {
+
+        PositionGeo geo;
+        int x;
+        int y;
+
+        public Corner(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.geo = HeightMap.this.getPosition(x, y);
+        }
+
+        public Position getPosition() {
+            double[] retvalX = new double[1];
+            double[] retvalY = new double[1];
+
+            gdal.ApplyGeoTransform(dataset.GetGeoTransform(), x, y, retvalX,
+                    retvalY);
+
+            return new Position(retvalX[0], retvalY[0]);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ") = " + geo;
+        }
+    }
+
+    /**
+     * @return the origin of the map: a position at the southwest corner of the
+     *         map
+     */
+    private final Position getOrigin() {
+
+        List<Corner> positions = Arrays.asList(new Corner(0, 0),
+                new Corner(dataset.GetRasterXSize() - 1,
+                        dataset.GetRasterYSize() - 1),
+                new Corner(dataset.GetRasterXSize() - 1, 0),
+                new Corner(0, dataset.GetRasterYSize() - 1));
+
+        Comparator<Corner> minLatitude = new Comparator<Corner>() {
+
+            @Override
+            public int compare(Corner o1, Corner o2) {
+                return Double.compare(o1.geo.lat(), o2.geo.lat());
+            }
+
+        };
+
+        Comparator<Corner> minLongitude = new Comparator<Corner>() {
+
+            @Override
+            public int compare(Corner o1, Corner o2) {
+                return Double.compare(o1.geo.lon(), o2.geo.lon());
+            }
+
+        };
+        positions.sort(minLatitude);
+
+        positions = Arrays.asList(positions.get(0), positions.get(1));
+        positions.sort(minLongitude);
+
+        return positions.get(0).getPosition();
+    }
+
+    /**
+     * @param position
+     *            the geographical position in WGS84
+     * @return The projected position of the geographical position onto the
+     *         height map's projection.
+     * @throws IllegalArgumentException
+     *             if the projected position is not in the raster
+     *
+     **/
+    private Position getOrigin(PositionGeo position) {
+        PositionGeo retval = transformPosition(fromWgs84, position);
+
+        Position checkRaster = applyInvTransform(retval.x(), retval.y());
+
+        if (checkRaster.x < 0 || checkRaster.x > dataset.GetRasterXSize()
+                || checkRaster.y < 0
+                || checkRaster.y >= dataset.GetRasterYSize()) {
+            throw new IllegalArgumentException("The geographic position "
+                    + position + " is outside the height map");
+        }
+
+        return new Position(retval.x(), retval.y());
+    }
+
+    private PositionGeo getPosition(int x, int y) {
+        double[] geoX = new double[1];
+        double[] geoY = new double[1];
+
+        gdal.ApplyGeoTransform(dataset.GetGeoTransform(), x, y, geoX, geoY);
+
+        return transformPosition(toWgs84, new PositionGeo(geoX[0], geoY[0]));
+    }
+
     /**
      * Create a height map from the terrain file
      * 
@@ -115,14 +217,16 @@ public class HeightMap {
         if (!datasetProjection.GetLinearUnitsName().equalsIgnoreCase("Metre")) {
             throw new RuntimeException(
                     "Map " + path + " uses projection units not in meters");
-        } else if (datasetProjection.GetAxisOrientation(null, 0) != osr.OAO_East) {
-            throw new RuntimeException(
-                    "Map " + path + " uses projection with first axis not towards NORTH.");                
-        } else if (datasetProjection.GetAxisOrientation(null, 1) != osr.OAO_North) {
-            throw new RuntimeException(
-                    "Map " + path + " uses projection with second axis not towards EAST.");                
+        } else if (datasetProjection.GetAxisOrientation(null,
+                0) != osrConstants.OAO_East) {
+            throw new RuntimeException("Map " + path
+                    + " uses projection with first axis not towards NORTH.");
+        } else if (datasetProjection.GetAxisOrientation(null,
+                1) != osrConstants.OAO_North) {
+            throw new RuntimeException("Map " + path
+                    + " uses projection with second axis not towards EAST.");
         }
-        
+
         double[] datasetTransform = dataset.GetGeoTransform();
 
         datasetInvTransform = gdal.InvGeoTransform(datasetTransform);
@@ -205,11 +309,10 @@ public class HeightMap {
 
         double[] read = new double[1];
 
-        int error = rasterBand.ReadRaster(
-                Double.valueOf(Math.round(rasterX[0])).intValue(),
-                Double.valueOf(Math.round(rasterY[0])).intValue(), 1, 1, read);
+        int error = rasterBand.ReadRaster((int) (rasterX[0]),
+                (int) (rasterY[0]), 1, 1, read);
 
-        if (error == gdalconst.CE_None) {
+        if (error == gdalconstConstants.CE_None) {
             if (read[0] != noDataValue) {
                 retval = zScale * read[0] + zOffset;
             } else {
@@ -217,7 +320,7 @@ public class HeightMap {
                         + x + ", " + y + ": "
                         + transformPosition(toWgs84, scaledX, scaledY));
             }
-        } else if (error == gdalconst.CE_Warning) {
+        } else if (error == gdalconstConstants.CE_Warning) {
             System.err.println("HeightMap.getHeight(): warning no values for "
                     + x + ", " + y);
         } else {
@@ -227,100 +330,170 @@ public class HeightMap {
         return retval;
     }
 
+    private Position getRasterPoint(Position p) {
+        double scaledX = origin.x + p.x / xScale;
+        double scaledY = origin.y + p.y / yScale;
+
+        double[] rasterX = new double[1];
+        double[] rasterY = new double[1];
+
+        gdal.ApplyGeoTransform(datasetInvTransform, scaledX, scaledY, rasterX,
+                rasterY);
+
+        return new Position(rasterX[0], rasterY[0]);
+    }
+
+    private Position getProjectionOffsetPoint(int x, int y) {
+        double[] projectionX = new double[1];
+        double[] projectionY = new double[1];
+
+        gdal.ApplyGeoTransform(dataset.GetGeoTransform(), x, y, projectionX,
+                projectionY);
+
+        return new Position(projectionX[0] * xScale - origin.x,
+                projectionY[0] * yScale - origin.y);
+    }
+
+    private List<LineSegment> getEdgeSegments(Position p1, Position p2) {
+        List<LineSegment> retval = new ArrayList<LineSegment>();
+        Position rasterP1 = getRasterPoint(p1);
+        Position rasterP2 = getRasterPoint(p2);
+
+        int minX = (int) (Math.min(rasterP1.x, rasterP2.x));
+        int maxX = (int) (Math.max(rasterP1.x, rasterP2.x));
+        int minY = (int) (Math.min(rasterP1.y, rasterP2.y));
+        int maxY = (int) (Math.max(rasterP1.y, rasterP2.y));
+
+        for (int x = minX; x <= maxX; ++x) {
+            for (int y = minY; y <= maxY; ++y) {
+                Position tl = getProjectionOffsetPoint(x, y);
+                Position tr = getProjectionOffsetPoint(x + 1, y);
+                Position bl = getProjectionOffsetPoint(x, y + 1);
+
+                retval.add(new LineSegment(tl, tr));
+                retval.add(new LineSegment(tl, bl));
+            }
+        }
+
+        return retval;
+    }
+
+    private void addEdgePoints(List<Position> edgePoints, final Position p1,
+            final Position p2, List<LineSegment> edgeSegments) {
+        LineSegment path = new LineSegment(p1, p2);
+
+        for (LineSegment edgeSegment : edgeSegments) {
+            try {
+                Position edgePoint = path.GetIntersection(edgeSegment);
+                edgePoint.z = Double.NaN;
+                edgePoints.add(edgePoint);
+            } catch (LineSegment.NoIntersectionException e) {
+            } catch (LineSegment.ParallelLinesException e) {
+            }
+        }
+
+        // Sort edge points
+        Comparator<Position> edgePointSorter = new Comparator<Position>() {
+
+            @Override
+            public int compare(Position arg0, Position arg1) {
+                int retval = 0;
+                if (p1.x < p2.x) {
+                    retval = Double.compare(arg0.x, arg1.x);
+                } else {
+                    retval = Double.compare(arg1.x, arg0.x);
+                }
+
+                if (retval == 0) {
+                    if (p1.y < p2.y) {
+                        retval = Double.compare(arg0.y, arg1.y);
+                    } else {
+                        retval = Double.compare(arg1.y, arg0.y);
+                    }
+                }
+
+                return retval;
+            }
+        };
+        edgePoints.sort(edgePointSorter);
+
+    }
+
     /**
-     * @param position
-     *            the geographical position in WGS84
-     * @return The projected position of the geographical position onto the
-     *         height map's projection.
-     * @throws IllegalArgumentException
-     *             if the projected position is not in the raster
-     *
-     **/
-    private Position getOrigin(PositionGeo position) {
-        PositionGeo retval = transformPosition(fromWgs84, position);
-
-        Position checkRaster = applyInvTransform(retval.x(), retval.y());
-
-        if (checkRaster.x < 0 || checkRaster.x > dataset.GetRasterXSize() || checkRaster.y < 0
-                || checkRaster.y >= dataset.GetRasterYSize()) {
-            throw new IllegalArgumentException("The geographic position "
-                    + position + " is outside the height map");
-        }
-
-        return new Position(retval.x(), retval.y());
-    }
-    
-    
-    private PositionGeo getPosition(int x, int y) {
-        double[] geoX = new double[1];
-        double[] geoY = new double[1];
-
-        gdal.ApplyGeoTransform(dataset.GetGeoTransform(), x, y, geoX, geoY);
-        
-        return transformPosition(toWgs84, new PositionGeo(geoX[0], geoY[0]));
-    }
-
-    private class Corner {
-
-        PositionGeo geo;
-        int x;
-        int y;
-        
-        public Corner(int x, int y) {
-            this.x = x;
-            this.y = y;
-            this.geo = HeightMap.this.getPosition(x, y);
-        }
-        
-        public Position getPosition() {
-            double[] retvalX = new double[1];
-            double[] retvalY = new double[1];
-            
-            gdal.ApplyGeoTransform(dataset.GetGeoTransform(), x, y, retvalX,
-                    retvalY);
-            
-            return new Position(retvalX[0], retvalY[0]);
-        }
-        
-        @Override
-        public String toString() {
-            return "(" + x + ", " + y + ") = " + geo; 
-        }
-    }
-    
-    /**
-     * @return the origin of the map: a position at the southwest corner of the map
+     * Get a list of positions on the path between p1 and p2 such that:
+     * <ul>
+     * <li>p1 and p2 are at the ends of the list</li>
+     * <li>positions other than p1 and p2 are either at the edges of a raster
+     * pixel (edge point) or at the midpoint between to positions at the edges
+     * of a raster pixel (center point).</li>
+     * <li>the height of each center point is the value in that raster
+     * pixel</li>
+     * <li>the height of each edge point is an average of the two adjoining
+     * center points</li> </ual>
+     * 
+     * @param p1
+     *            The position at the start of the path
+     * @param p2
+     *            The position at the end of the path
+     * @return
      */
-    private final Position getOrigin() {
-        
-        List<Corner> positions = Arrays.asList(
-                new Corner(0, 0),
-                new Corner(dataset.GetRasterXSize() - 1, dataset.GetRasterYSize() - 1),
-                new Corner(dataset.GetRasterXSize() - 1, 0),
-                new Corner(0, dataset.GetRasterYSize() - 1));                
+    public List<Position> getPath(Position p1, Position p2) {
 
-        Comparator<Corner> minLatitude = new Comparator<Corner>() {
+        List<Position> retval = new ArrayList<Position>();
 
-            @Override
-            public int compare(Corner o1, Corner o2) {
-                return Double.compare(o1.geo.lat(), o2.geo.lat());
+        addEdgePoints(retval, p1, p2, getEdgeSegments(p1, p2));
+
+        List<Position> centerPoints = new ArrayList<Position>();
+
+        for (int i = 0; i < retval.size() - 1; ++i) {
+
+            Position midPoint = LineSegment.midPoint(retval.get(i),
+                    retval.get(i + 1));
+            midPoint.z = getHeight(midPoint);
+            centerPoints.add(midPoint);
+        }
+
+        for (int i = centerPoints.size(); i > 0; --i) {
+            retval.add(i, centerPoints.get(i - 1));
+        }
+
+        retval.add(0, p1);
+        retval.add(p2);
+
+        Position prev = null;
+
+        for (Position position : retval) {
+
+            if (prev != null && Double.isNaN(prev.z)
+                    && !Double.isNaN(position.z)) {
+                prev.z = position.z;
             }
 
-        };
+            prev = position;
+        }
 
-        Comparator<Corner> minLongitude = new Comparator<Corner>() {
+        return retval;
+    }
 
-            @Override
-            public int compare(Corner o1, Corner o2) {
-                return Double.compare(o1.geo.lon(), o2.geo.lon());
+    /**
+     * Compute the distance between p1 and p2 on the map including the
+     * intermediate heights.
+     * 
+     * @param p1
+     * @param p2
+     * @return
+     */
+    public double getDistance(Position p1, Position p2) {
+        double retval = 0.0;
+        Position previous = null;
+
+        for (Position position : getPath(p1, p2)) {
+            if (previous != null) {
+                retval += previous.distance(position);
             }
+            previous = position;
+        }
 
-        };
-        positions.sort(minLatitude);
-
-        positions = Arrays.asList(positions.get(0), positions.get(1));
-        positions.sort(minLongitude);
-
-        return positions.get(0).getPosition();
+        return retval;
     }
 }
---
+++ src/com/perspectalabs/bonnmotion/util/LineSegment.java
@@ -0,0 +1,272 @@
+/**
+ * 
+ */
+package com.perspectalabs.bonnmotion.util;
+
+import edu.bonn.cs.iv.bonnmotion.Position;
+
+/**
+ * A class that represents a line segment with the ability to compute
+ * intersections.
+ * 
+ * @author ygottlieb
+ *
+ */
+public class LineSegment {
+
+    /**
+     * How close distances are to be considered 0.
+     */
+    public static double EPSILON = 1e-5;
+
+    private Position p1;
+    private Position p2;
+
+    private double slope;
+    private double yIntercept;
+
+    /**
+     * Create a new line segment between the positions.
+     * 
+     * @param p1
+     * @param p2
+     */
+    public LineSegment(Position p1, Position p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+
+        if (Math.abs(p2.x - p1.x) < EPSILON) {
+            slope = Double.POSITIVE_INFINITY;
+        } else {
+            slope = (p2.y - p1.y) / (p2.x - p1.x);
+        }
+
+        if (Double.isFinite(slope)) {
+            yIntercept = p1.y - slope * p1.x;
+        }
+    }
+
+    /**
+     * Exception thrown when two line segments do not intersect, but are not
+     * parallel.
+     * 
+     * @author ygottlieb
+     *
+     */
+    public static class NoIntersectionException extends Exception {
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 5511931877940653676L;
+
+    }
+
+    /**
+     * Exception thrown when two line segments do not intersect and are
+     * parallel.
+     * 
+     * @author ygottlieb
+     *
+     */
+    public static class ParallelLinesException extends Exception {
+
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 3988498536102069282L;
+
+    }
+
+    /**
+     * Compute the Y-coordinate of the line corresponding the given x. The
+     * returned value may not be on the segment.
+     * 
+     * @param x
+     * @return
+     */
+    private double GetY(double x) {
+        double retval = Double.NaN;
+
+        if (Double.isFinite(slope)) {
+            retval = slope * x + yIntercept;
+        }
+
+        return retval;
+    }
+
+    /**
+     * Does the given position lie in the section of the plane bounded by the
+     * segement's endpoints.
+     * 
+     * @param p
+     * @return
+     */
+    private boolean isBetweenEndpoints(Position p) {
+        return ((p1.x <= p.x && p.x <= p2.x) || (p2.x <= p.x && p.x <= p1.x))
+                && ((p1.y <= p.y && p.y <= p2.y)
+                        || (p2.y <= p.y && p.y <= p1.y));
+    }
+
+    /**
+     * Is this segment parallel to other?
+     * 
+     * @param other
+     * @return
+     */
+    private boolean isParallel(LineSegment other) {
+        boolean retval = false;
+
+        if (Double.isInfinite(slope)) {
+            retval = Double.isInfinite(other.slope);
+        } else if (Double.isInfinite(other.slope)) {
+            retval = false;
+        } else {
+            retval = Math.abs(slope - other.slope) < EPSILON;
+        }
+
+        return retval;
+    }
+
+    /**
+     * Return the intersection point of the two line segments.
+     * 
+     * @param other
+     * @return
+     * @throws NoIntersectionException
+     * @throws ParallelLinesException
+     */
+    public Position GetIntersection(LineSegment other)
+            throws NoIntersectionException, ParallelLinesException {
+        Position retval = new Position();
+
+        if (isParallel(other)) {
+            throw new ParallelLinesException();
+        } else if (Double.isInfinite(slope)) {
+            retval = other.GetIntersection(this);
+        } else if (Double.isInfinite(other.slope)) {
+            retval.x = other.p1.x;
+            retval.y = GetY(retval.x);
+        } else {
+            retval.x = (this.yIntercept - other.yIntercept)
+                    / (other.slope - this.slope);
+            retval.y = GetY(retval.x);
+        }
+
+        if (!isBetweenEndpoints(retval) || !other.isBetweenEndpoints(retval)) {
+            throw new NoIntersectionException();
+        }
+
+        return retval;
+    }
+
+    /**
+     * Do the lines intersect?
+     * 
+     * @param other
+     * @return
+     */
+    public boolean DoIntersect(LineSegment other) {
+        boolean retval = false;
+
+        try {
+            GetIntersection(other);
+            retval = true;
+        } catch (ParallelLinesException e) {
+        } catch (NoIntersectionException e) {
+        }
+
+        return retval;
+    }
+
+    /**
+     * Return the midpoint of the segment
+     * 
+     * @return
+     */
+    public Position midPoint() {
+        return midPoint(p1, p2);
+    }
+
+    /**
+     * Return the point on equidistant from p1 and p2 on the line between them.
+     * 
+     * @param p1
+     * @param p2
+     * @return
+     */
+    public static Position midPoint(Position p1, Position p2) {
+        return new Position((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(p1.toString()).append("->").append(p2)
+                .toString();
+    }
+
+    /*************************************************************************/
+    /* Some tests
+     */
+    private static void testIntersection() {
+        Position p1 = new Position(0, 0);
+        Position p2 = new Position(1, 1);
+        Position p3 = new Position(1, 0);
+        Position p4 = new Position(0, 1);
+
+        LineSegment l1 = new LineSegment(p1, p2);
+        LineSegment l2 = new LineSegment(p3, p4);
+
+        try {
+            System.out.println(l1.GetIntersection(l2).toString());
+            System.out.println(l2.GetIntersection(l1).toString());
+        } catch (ParallelLinesException e) {
+            System.out.println("Lines are parallel");
+        } catch (NoIntersectionException e) {
+            System.out.println("Line segments do not intersect");
+        }
+    }
+
+    private static void testVerticalIntersection() {
+        Position p1 = new Position(0.5, 0);
+        Position p2 = new Position(0.5, 1);
+        Position p3 = new Position(0, 0.3);
+        Position p4 = new Position(1, 0);
+
+        LineSegment l1 = new LineSegment(p1, p2);
+        LineSegment l2 = new LineSegment(p3, p4);
+
+        try {
+            System.out.println(l1.GetIntersection(l2).toString());
+            System.out.println(l2.GetIntersection(l1).toString());
+        } catch (ParallelLinesException e) {
+            System.out.println("Lines are parallel");
+        } catch (NoIntersectionException e) {
+            System.out.println("Line segments do not intersect");
+        }
+    }
+
+    private static void testNoIntersection() {
+        Position p1 = new Position(0, 0);
+        Position p2 = new Position(0, 1);
+        Position p3 = new Position(0.1, 0);
+        Position p4 = new Position(1, 0);
+
+        LineSegment l1 = new LineSegment(p1, p2);
+        LineSegment l2 = new LineSegment(p3, p4);
+
+        try {
+            System.out.println(l1.GetIntersection(l2).toString());
+            System.out.println(l2.GetIntersection(l1).toString());
+        } catch (ParallelLinesException e) {
+            System.out.println("Lines are parallel");
+        } catch (NoIntersectionException e) {
+            System.out.println("Line segments do not intersect");
+        }
+    }
+
+    public static void main(String argv[]) {
+        testIntersection();
+        testVerticalIntersection();
+        testNoIntersection();
+    }
+}
